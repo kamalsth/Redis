093fcf55fcf8e98ba96320fefe98c59be95e33a9
Kamal Shrestha <kamal@lancemeup.com>
2023-08-02 00:30:37 +0545

    User entity created

---
+++ src/main/java/com/springboot/redis/entities/User.java
@@ -0,0 +1,21 @@
+package com.springboot.redis.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("User")
+public class User implements Serializable {
+    @Id
+    private Long userId;
+    private String name;
+    private String address;
+}
